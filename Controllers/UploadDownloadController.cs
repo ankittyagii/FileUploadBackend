ea2e8d5312e886bebeed2889fd6748f5ae612643
AN298912 <ankit.tyagi2@wipro.com>
2020-09-02 14:40:07 +0300

    intial commit

---
+++ Controllers/UploadDownloadController.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace BackEnd.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UploadDownloadController : ControllerBase
+    {
+        private IHostingEnvironment _hostingEnvironment;
+
+        public UploadDownloadController(IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            string filePath = string.Empty;
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+            if (!Directory.Exists(uploads))
+            {
+                Directory.CreateDirectory(uploads);
+            }
+            if (file.Length > 0)
+            {
+                filePath = Path.Combine(uploads, file.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+            if (ExecuteExcel(filePath))
+                return Ok();
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        [Route("download")]
+        public async Task<IActionResult> Download([FromQuery] string file)
+        {
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+            var filePath = Path.Combine(uploads, file);
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+
+            return File(memory, GetContentType(filePath), file);
+        }
+
+        [HttpGet]
+        [Route("files")]
+        public IActionResult Files()
+        {
+            var result = new List<string>();
+
+            var uploads = Path.Combine(_hostingEnvironment.WebRootPath, "uploads");
+            if (Directory.Exists(uploads))
+            {
+                var provider = _hostingEnvironment.ContentRootFileProvider;
+                foreach (string fileName in Directory.GetFiles(uploads))
+                {
+                    var fileInfo = provider.GetFileInfo(fileName);
+                    result.Add(fileInfo.Name);
+                }
+            }
+            return Ok(result);
+        }
+
+
+        private string GetContentType(string path)
+        {
+            var provider = new FileExtensionContentTypeProvider();
+            string contentType;
+            if (!provider.TryGetContentType(path, out contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return contentType;
+        }
+
+        public bool ExecuteExcel(string path)
+        {
+            bool status = false;
+            try
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkBook;
+                xlWorkBook = xlApp.Workbooks.Open(path);
+                xlApp.Run("OIRMacro");
+
+                xlWorkBook.Save();
+                xlWorkBook.Close(true);
+                xlApp.Quit();
+                releaseObject(xlApp);
+                releaseObject(xlWorkBook);
+                status = true;
+            }
+            catch
+            {
+                status = false;
+            }
+            return status;
+        }
+
+        private static void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
